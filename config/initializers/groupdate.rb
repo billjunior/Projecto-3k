15f0e83100c6621a9c7318fc0c43881315e21988
Joaquim Junior <billjunior24@gmail.com>
2025-12-26 11:47:28 +0100

    Fix: Add Groupdate initializer for proper timezone configuration

---
+++ config/initializers/groupdate.rb
@@ -0,0 +1,6 @@
+# Groupdate configuration
+# Timezone for grouping dates
+Groupdate.time_zone = "Luanda" # Angola timezone (WAT - West Africa Time)
+
+# Week start (0 = Sunday, 1 = Monday)
+Groupdate.week_start = :mon
